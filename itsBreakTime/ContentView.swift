390d97f5cc7f2c6957a6bc7d3d70acd6360fac3f
yukongoldpotato <nokiakaz@gmail.com>
2023-10-22 16:50:35 +0100

    Initial Commit

---
+++ itsBreakTime/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  itsBreakTime
+//
+//  Created by Kazuki Minami on 2023/10/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ itsBreakTime/itsBreakTimeApp.swift
@@ -0,0 +1,17 @@
+//
+//  itsBreakTimeApp.swift
+//  itsBreakTime
+//
+//  Created by Kazuki Minami on 2023/10/22.
+//
+
+import SwiftUI
+
+@main
+struct itsBreakTimeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
