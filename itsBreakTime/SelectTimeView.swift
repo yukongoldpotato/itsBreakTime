b58db993ab88ffbc219e5f3d3320175ae5b652c6
yukongoldpotato <nokiakaz@gmail.com>
2023-10-23 16:35:53 +0100

    Created the basics

---
+++ itsBreakTime/SelectTimeView.swift
@@ -0,0 +1,18 @@
+//
+//  SelectTimeView.swift
+//  itsBreakTime
+//
+//  Created by Kazuki Minami on 2023/10/22.
+//
+
+import SwiftUI
+
+struct SelectTimeView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    SelectTimeView()
+}
